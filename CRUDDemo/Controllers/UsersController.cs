a8f0c8d6e880269e20cdb99b11366e0c173c3313
shassaan <syedhassaan.103@gmail.com>
2020-08-22 12:07:50 +0500

    added list of users

--- CRUDDemo/Controllers/UsersController.cs
+++ CRUDDemo/Controllers/UsersController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using CRUDDemo.Models;
 using CRUDDemo.ViewModels;
 using Microsoft.AspNetCore.Mvc;
@@ -11,16 +12,17 @@ namespace CRUDDemo.Controllers
     public class UsersController : Controller
     {
         private readonly ApplicationDbContext _db;
-         
-        public UsersController(ApplicationDbContext _db)
+        private readonly IMapper _mapper;
+        public UsersController(ApplicationDbContext _db,IMapper _mapper)
         {
             this._db = _db;
+            this._mapper = _mapper;
         }
         public IActionResult Index()
         {
             var users = _db.Users.ToList();
-            //var UsersVM = 
-            return View(users);
+            var UsersVM = _mapper.Map<List<UserVM>>(users);
+            return View(UsersVM);
         }
     }
 }
