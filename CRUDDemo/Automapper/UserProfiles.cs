028a4a1758e544bcf3dacb9699bf6cc698078777
shassaan <syedhassaan.103@gmail.com>
2020-08-22 11:52:22 +0500

    automapper added and userprofile created

---
+++ CRUDDemo/Automapper/UserProfiles.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using CRUDDemo.Models;
+using CRUDDemo.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDDemo.Automapper
+{
+    public class UserProfiles:Profile
+    {
+        public UserProfiles()
+        {
+            CreateMap<User, UserVM>().ReverseMap();
+            //CreateMap<User, UserVM>();
+        }
+    }
+}
--- CRUDDemo/Controllers/UsersController.cs
+++ CRUDDemo/Controllers/UsersController.cs
@@ -2,20 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CRUDDemo.Models;
+using CRUDDemo.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CRUDDemo.Controllers
 {
     public class UsersController : Controller
     {
-
-        public UsersController()
+        private readonly ApplicationDbContext _db;
+        public UsersController(ApplicationDbContext _db)
         {
-
+            this._db = _db;
         }
         public IActionResult Index()
         {
-            return View();
+            var users = _db.Users.ToList();
+
+            return View(users);
         }
     }
 }
